1c16573fb9d2f17a877da760a1eff94c041b2e83
Kelsey <37933054+kelman7000@users.noreply.github.com>
2023-09-23 14:38:34 -0600

    Cleaned up edit character dialog

--- PCCharacterManager/ViewModels/DialogWindowViewModels/DialogWindowEditCharacterViewModel.cs
+++ PCCharacterManager/ViewModels/DialogWindowViewModels/DialogWindowEditCharacterViewModel.cs
@@ -16,176 +16,42 @@ namespace PCCharacterManager.ViewModels
 	public class DialogWindowEditCharacterViewModel : ObservableObject
 	{
 		private readonly Window window;
-		private readonly DnD5eCharacter character;
-
-		public DnD5eCharacter Character
-		{
-			get { return character; }
-		}
 
 		public Array HitDice { get; } = Enum.GetValues(typeof(HitDie));
-
-		private string language;
-		public string Language
-		{
-			get { return language; }
-			set { OnPropertyChanged(ref language, value); }
-		}
-		private string weapon;
-		public string Weapon
-		{
-			get { return weapon; }
-			set { OnPropertyChanged(ref weapon, value); }
-		}
-		private string armor;
-		public string Armor
-		{
-			get { return armor; }
-			set { OnPropertyChanged(ref armor, value); }
-		}
-		private string tool;
-		public string Tool
-		{
-			get { return tool; }
-			set { OnPropertyChanged(ref tool, value); }
-		}
-		private string otherProf;
-		public string OtherProf
-		{
-			get { return otherProf; }
-			set { OnPropertyChanged(ref otherProf, value); }
-		}
-		public Property movement;
-		public Property Movement
-		{
-			get { return movement; }
-			set { OnPropertyChanged(ref movement, value); }
-		}
-
+		public DnD5eCharacter Character { get; }
+		public PropertyListViewModel MovementTypesListVM { get; }
+		public StringListViewModel LanguagesVM { get; }
+		public StringListViewModel ArmorProfsVM { get; }
+		public StringListViewModel WeaponProfsVM { get; }
+		public StringListViewModel ToolProfsVM { get; }
+		public StringListViewModel OtherProfsVM { get; }
+		
 		public ICommand OkCommand { get; }
-
-		public ICommand AddLanguageCommand { get; }
-		public ICommand RemoveLanguageCommand { get; }
-		public ICommand AddMovementCommand { get; }
-		public ICommand RemoveMovementCommand { get; }
-		public ICommand AddWeaponCommand { get; }
-		public ICommand RemoveWeaponCommand { get; }
-		public ICommand AddArmorCommand { get; }
-		public ICommand RemoveArmorCommand { get;  }
-		public ICommand AddToolCommand { get; }
-		public ICommand RemoveToolCommand { get; }
-		public ICommand AddOtherProfCommand { get; }
-		public ICommand RemoveOtherProfCommand { get; }
 
 		public DialogWindowEditCharacterViewModel(Window _window, DnD5eCharacter _character)
 		{
 			window = _window;
-			character = _character;
+			Character = _character;
 
 			OkCommand = new RelayCommand(Ok);
 
-			language = string.Empty;
-			weapon = string.Empty;
-			armor = string.Empty;
-			tool = string.Empty;
-			otherProf = string.Empty;
-			movement = new Property();
-
-			AddLanguageCommand = new RelayCommand(AddLanguage);
-			RemoveLanguageCommand = new RelayCommand(RemoveLanguage);
-			AddMovementCommand = new RelayCommand(AddMovement);
-			RemoveMovementCommand = new RelayCommand(RemoveMovement);
-			AddWeaponCommand = new RelayCommand(AddWeapon);
-			RemoveWeaponCommand = new RelayCommand(RemoveWeapon);
-			AddArmorCommand = new RelayCommand(AddArmor);
-			RemoveArmorCommand = new RelayCommand(RemoveArmor);
-			AddToolCommand = new RelayCommand(AddTool);
-			RemoveToolCommand = new RelayCommand(RemoveTool);
-			AddOtherProfCommand = new RelayCommand(AddOtherProf);
-			RemoveOtherProfCommand = new RelayCommand(RemoveOtherProf);
+			MovementTypesListVM = new PropertyListViewModel("Movement", Character.MovementTypes_Speeds);
+			LanguagesVM = new StringListViewModel("Languages", Character.Languages);
+			ToolProfsVM = new StringListViewModel("Tool Profs", Character.ToolProficiences);
+			ArmorProfsVM = new StringListViewModel("Armor Profs", Character.ArmorProficiencies);
+			OtherProfsVM = new StringListViewModel("Other Profs", Character.OtherProficiences);
+			WeaponProfsVM = new StringListViewModel("Weapon Profs", Character.WeaponProficiencies);
+			OnPropertyChanged(nameof(MovementTypesListVM));
+			OnPropertyChanged(nameof(LanguagesVM));
+			OnPropertyChanged(nameof(ArmorProfsVM));
+			OnPropertyChanged(nameof(WeaponProfsVM));
+			OnPropertyChanged(nameof(ToolProfsVM));
+			OnPropertyChanged(nameof(OtherProfsVM));
 		}
 
 		public void Ok()
 		{
 			window.Close();
-		}
-
-		public void AddLanguage()
-		{
-			AddTo(character.Languages);
-		}
-		public void RemoveLanguage()
-		{
-			character.Languages.Remove(language);
-		}
-		public void AddMovement()
-		{
-			Window window = new StringInputDialogWindow();
-			DialogWindowStringInputViewModel windowVM = new DialogWindowStringInputViewModel(window, "Movement type i.e. FLY");
-			window.DataContext = windowVM;
-			window.ShowDialog();
-
-			if (window.DialogResult == false)
-				return;
-
-			Window window1 = new StringInputDialogWindow();
-			DialogWindowStringInputViewModel windowVM1 = new DialogWindowStringInputViewModel(window1, "Movement Speed i.e. 30ft");
-			window1.DataContext = windowVM1;
-			window1.ShowDialog();
-
-			if (window1.DialogResult == false)
-				return;
-
-			character.MovementTypes_Speeds.Add(new Property(windowVM.Answer, windowVM1.Answer));
-		}
-		public void RemoveMovement()
-		{
-			character.MovementTypes_Speeds.Remove(movement);
-		}
-		public void AddWeapon()
-		{
-			AddTo(character.WeaponProficiencies);
-		}
-		public void RemoveWeapon()
-		{
-			character.WeaponProficiencies.Remove(weapon);
-		}
-		public void AddArmor()
-		{
-			AddTo(character.ArmorProficiencies);
-		}
-		public void RemoveArmor()
-		{
-			character.ArmorProficiencies.Remove(armor);
-		}
-		public void AddTool()
-		{
-			AddTo(character.ToolProficiences);
-		}
-		public void RemoveTool()
-		{
-			character.ToolProficiences.Remove(tool);
-		}
-		public void AddOtherProf()
-		{
-			AddTo(character.OtherProficiences);
-		}
-		public void RemoveOtherProf()
-		{
-			character.OtherProficiences.Remove(otherProf);
-		}
-
-		private void AddTo(in ObservableCollection<string> addTo)
-		{
-			Window window = new StringInputDialogWindow();
-			DialogWindowStringInputViewModel windowVM = new DialogWindowStringInputViewModel(window);
-			window.DataContext = windowVM;
-			window.ShowDialog();
-
-			if (window.DialogResult == false)
-				return;
-
-			addTo.Add(windowVM.Answer);
 		}
 	}
 }
